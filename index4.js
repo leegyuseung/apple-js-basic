24f9d83d9107d9b50af27237c97062a3c4aee36b
gyuseung lee <lgs4002@naver.com>
2023-02-03 23:16:18 +0900

    이벤트 버블링 응용과 dataset

--- index4.js
+++ index4.js
@@ -1,10 +1,19 @@
-for (let i = 0; i < 3; i++) {
-  $(".tab-button")
-    .eq(i)
-    .on("click", function () {
-      $(".tab-button").removeClass("orange");
-      $(".tab-button").eq(i).addClass("orange");
-      $(".tab-content").removeClass("show");
-      $(".tab-content").eq(i).addClass("show");
-    });
+// for (let i = 0; i < 3; i++) {
+//   $(".tab-button")
+//     .eq(i)
+//     .on("click", function () {
+//       openTab(i);
+//     });
+// }
+$(".list").click(function (e) {
+  // if (e.target == document.querySelectorAll(".tab-button")[0]) {
+  openTab(e.target.dataset.id);
+  // }
+});
+
+function openTab(num) {
+  $(".tab-button").removeClass("orange");
+  $(".tab-button").eq(num).addClass("orange");
+  $(".tab-content").removeClass("show");
+  $(".tab-content").eq(num).addClass("show");
 }
